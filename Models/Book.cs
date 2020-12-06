6143a8fdcb873f46079eaa1f758ae14789c119c8
ANDREI-MIRCEA BUTEANU <buteanu.andrei154451@student.ubbcluj.ro>
2020-12-06 14:19:45 +0200

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buteanu_Andrei_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public decimal Price { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
